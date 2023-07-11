781f150421c7808d5d86032ad7f4d3c1a45cb016
Razo <bestwishtous@gmail.com>
2023-07-11 20:36:06 +0800

    feature: 脚本生效测试

--- Loon/Script/GithubProxy.js
+++ Loon/Script/GithubProxy.js
@@ -1,6 +1,13 @@
 (() => {
   const proxyUrl = `https://ghproxy.com`;
   $done({
-    url: `${proxyUrl}/${$request.url}`,
+    response: {
+      status: 200,
+      headers: {},
+      body: "",
+    },
   });
+  //   $done({
+  //     url: `${proxyUrl}/${$request.url}`,
+  //   });
 })();
